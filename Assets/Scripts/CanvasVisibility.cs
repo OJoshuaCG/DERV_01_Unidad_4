b281c37b3ee34305658707a472611a71c746aa5a
Joshua CG <joshuacarrasco99@gmail.com>
2021-12-13 07:52:53 -0600

    All activities update

--- Assets/Scripts/CanvasVisibility.cs
+++ Assets/Scripts/CanvasVisibility.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CanvasVisibility : MonoBehaviour
-{
+{   
+    // Enabled Desactiva/Activa Componentes dentro de un GameObject
+    // SetActive Activa/Desactiva a GameObjects
     [SerializeField]
     Canvas canvas;
 
+    [SerializeField]
+    GameObject canvas2;
+
     public void setEnableCanvas(bool state){
+        //Propiedad
         canvas.enabled = state;
+    }
+
+    public void setActiveCanvas(bool state){
+        //Metodo
+        canvas2.SetActive(state);
     }
 }
